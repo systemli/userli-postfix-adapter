f8b2be416071d9956d6c7b1458bb8a208a360a92
louis <louis@systemli.org>
2024-11-30 13:19:01 +0100

    ✨ Introduce Worker Pools for TCP Server

--- main.go
+++ main.go
@@ -22,10 +22,10 @@ func main() {
 	var wg sync.WaitGroup
 
 	wg.Add(4)
-	go StartTCPServer(ctx, &wg, config.AliasListenAddr, adapter.AliasHandler)
-	go StartTCPServer(ctx, &wg, config.DomainListenAddr, adapter.DomainHandler)
-	go StartTCPServer(ctx, &wg, config.MailboxListenAddr, adapter.MailboxHandler)
-	go StartTCPServer(ctx, &wg, config.SendersListenAddr, adapter.SendersHandler)
+	go StartTCPServer(ctx, &wg, config.AliasListenAddr, adapter.AliasHandler, config.AliasMaxWorkers)
+	go StartTCPServer(ctx, &wg, config.DomainListenAddr, adapter.DomainHandler, config.DomainMaxWorkers)
+	go StartTCPServer(ctx, &wg, config.MailboxListenAddr, adapter.MailboxHandler, config.MailboxMaxWorkers)
+	go StartTCPServer(ctx, &wg, config.SendersListenAddr, adapter.SendersHandler, config.SendersMaxWorkers)
 
 	wg.Wait()
 	log.Info("All servers stopped")
--- server.go
+++ server.go
@@ -8,8 +8,14 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
-func StartTCPServer(ctx context.Context, wg *sync.WaitGroup, addr string, handler func(net.Conn)) {
+func StartTCPServer(ctx context.Context, wg *sync.WaitGroup, addr string, handler func(net.Conn), maxWorkers int) {
 	defer wg.Done()
+
+	// Create a buffered channel to limit concurrent connections
+	semaphore := make(chan struct{}, maxWorkers)
+
+	// Create connection queue channel
+	connQueue := make(chan net.Conn, maxWorkers)
 
 	listener, err := net.Listen("tcp", addr)
 	if err != nil {
@@ -23,8 +29,14 @@ func StartTCPServer(ctx context.Context, wg *sync.WaitGroup, addr string, handle
 		listener.Close()
 	}()
 
-	log.Info("Server started on ", addr)
+	// Start worker pool
+	for i := 0; i < maxWorkers; i++ {
+		go worker(ctx, handler, connQueue, semaphore)
+	}
 
+	log.Info("Server started on ", addr, " with ", maxWorkers, " workers")
+
+	// Accept connections
 	for {
 		conn, err := listener.Accept()
 		if err != nil {
@@ -36,6 +48,34 @@ func StartTCPServer(ctx context.Context, wg *sync.WaitGroup, addr string, handle
 			continue
 		}
 
-		go handler(conn)
+		// Try to acquire semaphore
+		select {
+		case semaphore <- struct{}{}:
+			// Successfully acquired semaphore, queue the connection
+			select {
+			case connQueue <- conn:
+			case <-ctx.Done():
+				conn.Close()
+				return
+			}
+		default:
+			// If we can't acquire semaphore, we're at capacity
+			log.Warn("Server at capacity, dropping connection")
+			conn.Close()
+		}
+	}
+}
+
+func worker(ctx context.Context, handler func(net.Conn), connQueue chan net.Conn, semaphore chan struct{}) {
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		case conn := <-connQueue:
+			// Process the connection
+			handler(conn)
+			// Release the semaphore
+			<-semaphore
+		}
 	}
 }
